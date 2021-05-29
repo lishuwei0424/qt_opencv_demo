e10f6387558eb788cd5f699bcd19e8c445ad37ec
李书伟 <lishuwei@tju.edu.cn>
2021-05-29 12:51:07 +0800

    image Rect

---
+++ widget.h
@@ -0,0 +1,40 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QFile>
+#include <QFileDialog>
+#include <opencv2/opencv.hpp>
+#include <QMessageBox>
+#include "mylabel.h"
+#include <QPoint>
+using namespace cv;
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+private slots:
+    void open_path_slot();
+    void StartPointSlot(QPoint p);
+    void StopPointSlot(QPoint p);
+    void PointTextChangeSlot(QString);
+    void on_path_btn_clicked();
+
+private:
+    Ui::Widget *ui;
+    void show_img(Mat image,QLabel *label);
+    MyLabel *label_img;
+    VideoCapture *capture=NULL;
+    QPoint StartPoint;
+    double img_w ;
+    double img_h ;
+};
+
+#endif // WIDGET_H
